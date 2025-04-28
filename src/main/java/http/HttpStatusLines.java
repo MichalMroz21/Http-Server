105a6f8dac62df02e936ae0a9e7a607040cf30d6
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2025-04-29 00:58:09 +0200

    Added file handling 5

--- src/main/java/http/HttpStatusLines.java
+++ src/main/java/http/HttpStatusLines.java
@@ -2,6 +2,6 @@ package http;
 
 public class HttpStatusLines {
     public static final String OK = "HTTP/1.1 200 OK\r\n";
-    public static final String NOT_FOUND = "HTTP/1.1 404 Not Found\r\n";
-    public static final String INTERNAL_SERVER_ERROR = "HTTP/1.1 500 Internal Server Error\r\n";
+    public static final String NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n";
+    public static final String INTERNAL_SERVER_ERROR = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
 }
